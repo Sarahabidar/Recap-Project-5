ec45460e00868288f3a1844d974a22cfbb8a4862
sara <sarah.abiidar@gmail.com>
2024-06-13 16:08:10 +0200

    adde back-button to detail-page

--- Components/ArtPieceDetails.js
+++ Components/ArtPieceDetails.js
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+
 export default function ArtPieceDetails({ image, title, artist, year, genre }) {
+  const router = useRouter();
+  const handleBackButton = () => {
+    router.back();
+  };
+
   return (
     <div>
       <Image src={image} alt={title} width={100} height={100}></Image>
@@ -7,6 +14,7 @@ export default function ArtPieceDetails({ image, title, artist, year, genre }) {
       <p>Artist: {artist}</p>
       <p>Year: {year}</p>
       <p>Genre: {genre}</p>
+      <button onClick={handleBackButton}>Go Back to List</button>
     </div>
   );
 }
